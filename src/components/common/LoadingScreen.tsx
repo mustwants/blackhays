3a5261716511598aacda89dfc4aa18a400984e00
MustWants <90696663+mustwants@users.noreply.github.com>
2025-06-20 22:12:12 -0400

    Updated LoadingScreen.tsx

---
+++ src/components/common/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+const LoadingScreen = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-white">
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingScreen;
