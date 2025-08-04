e21e2790d18e59b8b4daf9f233f15942a09dffcc
MustWants <90696663+mustwants@users.noreply.github.com>
2025-08-04 14:52:48 -0400

    Update 20250701000000_add_company_media_social.sql

--- supabase/migrations/20250701000000_add_company_media_social.sql
+++ supabase/migrations/20250701000000_add_company_media_social.sql
@@ -1,1 +1,7 @@
-
+-- Add media and social link fields for company submissions
+ALTER TABLE company_submissions
+  ADD COLUMN logo_url text,
+  ADD COLUMN product_image_url text,
+  ADD COLUMN linkedin text,
+  ADD COLUMN twitter text,
+  ADD COLUMN facebook text;
