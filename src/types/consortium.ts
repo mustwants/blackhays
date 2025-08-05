4eb194575da3f64182e5aa55219eac95eea8c4fb
MustWants <90696663+mustwants@users.noreply.github.com>
2025-08-05 12:25:47 -0400

    Create consortium.ts

---
+++ src/types/consortium.ts
@@ -0,0 +1,9 @@
+export interface Consortium {
+  id: string;
+  name: string;
+  logo: string; // URL or path to logo image
+  website: string;
+  description: string;
+  details: string[];
+  focus: string[];
+}
