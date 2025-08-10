e4c32d08705731db2d541fc628f42866e2486d29
MustWants <90696663+mustwants@users.noreply.github.com>
2025-08-10 16:25:41 -0400

    Fix TailwindCSS PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+import tailwindConfig from './tailwind.config.js';
+
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': { config: tailwindConfig },
     autoprefixer: {},
   },
 };
