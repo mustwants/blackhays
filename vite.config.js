53e1141729c67f354ba4bb3fa2a2be75a632dcc5
MustWants <90696663+mustwants@users.noreply.github.com>
2025-08-12 16:00:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-﻿// vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5191,
-    strictPort: true,   // fail if 5191 is busy (don’t hop to 517x)
+    strictPort: true,
     host: '0.0.0.0'
   },
   preview: {
