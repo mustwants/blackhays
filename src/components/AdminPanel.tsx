b51953ec183d8e83a3a470c9cdd25c60f6e4a9f7
MustWants <90696663+mustwants@users.noreply.github.com>
2025-08-11 22:07:11 -0400

    Update AdminPanel.tsx

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
++ import { supabase } from '../supabaseClient';
 
 type Profile = {
   id: string;
